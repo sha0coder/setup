88a529a3a3711f22c459f1fee6beae317d42796e
sha0coder <sha0c0d3r@gmail.com>
2024-02-03 19:30:50 +0100

    some mac configs

---
+++ mac/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias l="ls -ltra"
+alias p="ps -ef"
+alias i="ifconfig | grep 192.168"
+alias n="lsof -i TCP -n -P"
+alias r="route -n get default"
