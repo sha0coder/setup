936d0f88e08d1270b91ee3b10ca47e723cddddcf
sha0coder <sha0c0d3r@gmail.com>
2024-02-04 19:28:10 +0100

    some linux configs

---
+++ linux/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias l="ls -ltra"
+alias p="ps auxf"
+alias n="sudo netstat -putan"
+
+export PATH="$HOME/.cargo/bin:$PATH"
