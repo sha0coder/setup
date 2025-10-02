9b9108d11f2e388d48eb75f4fb2eccce4e5a9c69
sha0coder <sha0@badchecksum.net>
2025-10-02 16:05:48 +0200

    install scripts improved

--- linux/root_install.sh
+++ linux/root_install.sh
@@ -14,7 +14,7 @@ apt install -y python3-pip
 apt install -y zlib1g zlib1g-dev 
 apt install -y unzip nodejs npm ffmpeg ranger mplayer strace ltrace valgrind
 npm install -g npm 
-apt install -y net-tools ufw readline-common wireshark tshark tcpdump tcpflow nautilus nitrogen
+apt install -y net-tools ufw readline-common wireshark tshark tcpdump tcpflow nautilus nitrogen arping arp-scan
 apt install -y keepassxc meld ranger kazam qemu-system
 apt install -y awesome awesome-cli awesome-extra checksec gdb i3lock jq sqlite3
 apt install -y flameshot
@@ -30,10 +30,13 @@ apt install -y mplayer
 apt install -y firmware-iwlwifi firmware-linux
 apt install -y python3-pip 
 apt install -y nmap yara python3-yara libyara-dev masscan dirbuster gobuster binwalk aircrack-ng 
+apt install -y default-jdk
+
 
 # cross-compilation to windows
 sudo apt install -y mingw-w64 mingw-w64-tools binutils-mingw-w64 \
   gcc-mingw-w64 g++-mingw-w64 wine64 wine
  
 
-
+# ollama
+curl -fsSL https://ollama.com/install.sh | sh
