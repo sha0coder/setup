65e9d6285ac0f748510c03b444e3bbd5283e76ee
sha0coder <sha0@badchecksum.net>
2025-11-03 11:23:59 +0100

    more fish aliases

--- linux/config.fish
+++ linux/config.fish
@@ -7,8 +7,10 @@ alias p="ps auxf"
 alias n="sudo netstat -punta"
 alias i="sudo ifconfig"
 alias d="sudo dmesg -T"
+alias en="setxkbmap us"
+alias es="setxkbmap es"
 export EDITOR="nvim"
-export PATH="$HOME/.cargo/bin:/home/sha0/.local/bin:/opt/zig/:$PATH"
+#export PATH="$HOME/.cargo/bin:/home/sha0/.local/bin:/opt/zig/:$PATH"
 
 
 
