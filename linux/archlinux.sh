8a44d5a712bababa9085512d6dd433bc68b00864
sha0coder <sha0@badchecksum.net>
2026-05-24 15:29:44 +0200

    hyprland config improvements

--- linux/archlinux.sh
+++ linux/archlinux.sh
@@ -4,15 +4,20 @@ sudo pacman -S hyprland hyprutils hyprshot hyprpaper hyprlock kitty hyprlauncher
 sudo pacman -S fish gcc gdb ltrace strace cmake rust cargo neovim git wget curl virtualbox chromium builder less tree irssi neovim fish
 sudo pacman -S wireguard-tools macchanger ufw whois unzip ffmpeg mplayer ranger valgrind 7zip nmap netcat python-pip binwalk yara gobuster aircrack-ng meld sqlite3 wireshark-qt wireshark-cli tcpdump tcpflow net-tools telegram-desktop
 mingw-w64-binutils mingw-w64-crt mingw-w64-gcc mingw-w64-headers mingw-w64-winpthreads 
-sudo pacman -S pipewire wireplumber wev cava
+sudo pacman -S pipewire wireplumber wev cava socat
 sudo pacman -S nerd-fonts v4l-utils kamoso remmina radare2 espeak-ng 
-sudo pacman -S ollama npm nodejs feh gnome-builder meson ninja clang obs-studio android-udev
+sudo pacman -S ollama npm nodejs feh gnome-builder meson ninja clang obs-studio android-udev wipe
+
+sudo pacman -S virtualbox
+sudo pacman -S virtualbox virtualbox-host-dkms dkms linux-headers
+sudo modprobe vboxdrv
 
 
 # yubi key
 sudo pacman -S ykman libfido2 pcscd
 systemctl enable --now pcscd
 
+# yay (to build form aur)
 cd
 mkdir soft
 cd soft
